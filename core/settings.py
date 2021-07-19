11207ee1e389f869208fe371b2ef0c2d248118a1
srdevelopervishal <s.r.developervishal@gmail.com>
2021-07-19 21:30:43 +0530

    update

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '4a-i=f57$_ivtv-7xln7gz43&=1_@c%=6=v^&0s7wkrfnjl%y!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://vammacademy.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['vammacademy.herokuapp.com','127.0.0.1']
 
 
 # Application definition
